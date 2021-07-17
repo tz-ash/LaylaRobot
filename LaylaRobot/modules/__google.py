32101d482121cbf781c5ed3e821cbc88be5de60c
Kid_of_devil <82517468+tz-ash@users.noreply.github.com>
2021-07-17 20:13:14 +0530

    Update __google.py

--- LaylaRobot/modules/__google.py
+++ LaylaRobot/modules/__google.py
@@ -286,6 +286,6 @@ __help__ = """
  ❍ /github <username>*:* Get information about a GitHub user.
  ❍ /country <country name>*:* Gathering info about given country
  ❍ /imdb <Movie name>*:* Get full info about a movie with imdb.com
- ❍ Yone <query>*:* Yone answers the query
+ ❍ Lyci <query>*:* Lyci answers the query
   💡Ex: `lyci where is India?`
 """
